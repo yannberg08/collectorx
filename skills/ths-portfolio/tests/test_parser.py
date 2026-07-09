3d6b5c9919f8722f44b6326a559c0cf57a639c65
yannberg08 <yannberg08@gmail.com>
2026-07-10 01:07:47 +0800

    Write THS test fixtures as UTF-8

--- skills/ths-portfolio/tests/test_parser.py
+++ skills/ths-portfolio/tests/test_parser.py
@@ -59,7 +59,7 @@ def test_parse_csv():
     test_csv.write_text("""成交日期,成交时间,证券代码,证券名称,买卖方向,成交价格,成交数量,成交金额,手续费,印花税,过户费
 2024-01-15,09:30:00,600519,贵州茅台,买入,1800.00,100,180000.00,54.00,0.00,1.80
 2024-01-16,10:00:00,600519,贵州茅台,卖出,1850.00,100,185000.00,55.50,185.00,1.85
-""")
+""", encoding="utf-8")
     
     records = parse_portfolio_csv(str(test_csv))
     
@@ -74,7 +74,7 @@ def test_parse_csv():
 def test_parse_empty_csv():
     """测试空CSV"""
     test_csv = ths_tmp("test_ths_empty_csv", "test_ths_empty.csv")
-    test_csv.write_text("成交日期,成交时间,证券代码,证券名称,买卖方向,成交价格,成交数量,成交金额,手续费,印花税,过户费\n")
+    test_csv.write_text("成交日期,成交时间,证券代码,证券名称,买卖方向,成交价格,成交数量,成交金额,手续费,印花税,过户费\n", encoding="utf-8")
     
     records = parse_portfolio_csv(str(test_csv))
     
@@ -88,7 +88,7 @@ def test_records_to_events():
     test_csv = ths_tmp("test_ths_events_csv", "test_ths_events.csv")
     test_csv.write_text("""成交日期,成交时间,证券代码,证券名称,买卖方向,成交价格,成交数量,成交金额,手续费,印花税,过户费
 2024-01-15,09:30:00,600519,贵州茅台,买入,1800.00,100,180000.00,54.00,0.00,1.80
-""")
+""", encoding="utf-8")
 
     records = parse_portfolio_csv(str(test_csv))
     events = records_to_events(
